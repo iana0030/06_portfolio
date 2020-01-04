b4274a71bc7fc8afc7a638017983ba59fad9c88d
iana0030 <iana0030@stud.kea.dk>
2020-01-04 17:23:28 +0100

    media queries, burgermenu

--- script.js
+++ script.js
@@ -2,4 +2,25 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
+
+    clickMenu();
+    document.querySelector("#menuknap").addEventListener("click", clickMenu);
+}
+
+
+function clickMenu() {
+    console.log("clickMenu");
+
+    document.querySelector("#burger").classList.remove("hidden");
+    document.querySelector("#menuknap").addEventListener("click", clickFjern);
+}
+
+
+function clickFjern() {
+    console.log("clickFjern");
+
+    document.querySelector("#burger").classList.add("hidden");
+
+    document.querySelector("#menuknap").removeEventListener("click", clickFjern);
+    document.querySelector("#menuknap").addEventListener("click", clickMenu);
 }
