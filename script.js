d8c759bb8f1da0665113df50a0162720ac8ae523
iana0030 <iana0030@stud.kea.dk>
2020-01-04 21:22:36 +0100

    portfolio side done

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ function sidenVises() {
     console.log("siden vises");
 
     document.querySelector("#menuknap").addEventListener("click", clickMenu);
-}
 
+    document.querySelector("#mappe_1").addEventListener("mouseover", hoverBig);
+    document.querySelector("#mappe_2").addEventListener("mouseover", hoverBig);
+    document.querySelector("#mappe_3").addEventListener("mouseover", hoverBig);
+    document.querySelector("#mappe_4").addEventListener("mouseover", hoverBig);
+}
 
 function clickMenu() {
     console.log("clickMenu");
     document.querySelector("#menuknap").removeEventListener("click", clickMenu);
+
+    document.querySelector("#menuknap").textContent = "X";
+
+    document.querySelector("section").classList.add("overlay");
 
     document.querySelector("#burger").classList.remove("hidden");
     document.querySelector("#menuknap").addEventListener("click", clickFjern);
@@ -21,6 +29,46 @@ function clickFjern() {
 
     document.querySelector("#burger").classList.add("hidden");
 
+    document.querySelector("#menuknap").textContent = "☰";
+
+    document.querySelector("section").classList.remove("overlay");
+
     document.querySelector("#menuknap").removeEventListener("click", clickFjern);
     document.querySelector("#menuknap").addEventListener("click", clickMenu);
+}
+
+function hoverBig() {
+    console.log("hover big");
+
+    this.classList.remove("bigger_reverse");
+    this.classList.add("bigger");
+
+    document.querySelector("#mappe_1").removeEventListener("mouseover", hoverBig);
+    document.querySelector("#mappe_1").addEventListener("mouseout", removeHoverBig);
+    document.querySelector("#mappe_2").removeEventListener("mouseover", hoverBig);
+    document.querySelector("#mappe_2").addEventListener("mouseout", removeHoverBig);
+    document.querySelector("#mappe_3").removeEventListener("mouseover", hoverBig);
+    document.querySelector("#mappe_3").addEventListener("mouseout", removeHoverBig);
+    document.querySelector("#mappe_4").removeEventListener("mouseover", hoverBig);
+    document.querySelector("#mappe_4").addEventListener("mouseout", removeHoverBig);
+}
+
+function removeHoverBig() {
+    console.log("hover big gone");
+
+    this.classList.remove("bigger");
+    this.classList.add("bigger_reverse");
+
+    document.querySelector("#mappe_1").addEventListener("mouseover", hoverBig);
+    document.querySelector("#mappe_1").removeEventListener("mouseout", removeHoverBig);
+    document.querySelector("#mappe_2").addEventListener("mouseover", hoverBig);
+    document.querySelector("#mappe_2").removeEventListener("mouseout", removeHoverBig);
+
+    document.querySelector("#mappe_3").addEventListener("mouseover", hoverBig);
+    document.querySelector("#mappe_3").removeEventListener("mouseout", removeHoverBig);
+
+    document.querySelector("#mappe_4").addEventListener("mouseover", hoverBig);
+    document.querySelector("#mappe_4").removeEventListener("mouseout", removeHoverBig);
+
+
 }
